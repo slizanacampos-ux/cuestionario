2b01fe51f8bbe9c6e5dacd26c05504f80c5993df
slizanacampos-ux <slizanacampos@gmail.com>
2026-07-07 11:17:45 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+window.APP_CONFIG = {
+  // URL terminada en /exec de la aplicación web de Google Apps Script.
+  GOOGLE_APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzvMMgrJJbXE3DyFQbGfyMXjWlTH-504dnbA0S4t2Do9mrq9LfJdW_ruGOVvHD-z29g/exec",
+
+  // URL privada del Google Sheet. Solo se usa en admin.html como acceso directo.
+  GOOGLE_SHEET_URL: "https://docs.google.com/spreadsheets/d/10lOkdl_k1w7Ef1jf8Q5OMxsCbF7PHYcvpEMZ97uVJ2I/edit?usp=sharing",
+
+  INSTRUMENT_VERSION: "3.0",
+  STUDY_TITLE: "Cuestionario sobre pensamiento probabilístico orientado a la toma de decisiones y conocimiento didáctico para la enseñanza de la probabilidad",
+  ADMIN_CONTACT: ""
+};
